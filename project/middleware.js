e844e5d1767e58e6b7b3460c90239c3112a1ccb6
Cemal Ayberk Coşgun <cemalayberkcosgun@gmail.com>
2024-07-25 17:07:15 +0300

    changed the user list from a json file to database
    added database support with docker

--- project/middleware.js
+++ project/middleware.js
@@ -1,9 +1,15 @@
-const fs = require("fs")
-const jwt = require("jsonwebtoken")
+import jwt from "jsonwebtoken"
+import mysql from "mysql2"
+import dotenv from "dotenv"
+dotenv.config()
 
-function updateJSON(userList){
-	fs.writeFileSync("./user_list.json", JSON.stringify(userList, null, "\t"), err =>{if(err) throw err;})
-}
+const db = mysql.createPool({
+	host:process.env.MYSQL_HOST,
+	port:process.env.MYSQL_PORT,
+	user:process.env.MYSQL_USER,
+	password:process.env.MYSQL_PASSWORD,
+	database:process.env.MYSQL_DATABASE
+}).promise()
 
 function checkUser(req, res, next) {
 	const authorization = req.get("Authorization")
@@ -14,8 +20,21 @@ function checkUser(req, res, next) {
 			next()}
 		else {throw err}
 	})}
-	else{res.status(403)}
+	else{next()}
 }
 
-module.exports.updateJSON = updateJSON
-module.exports.checkUser = checkUser
+async function getUser(email){
+	var a = await db.query(`select * from users where email = "${email}";`)
+	return a[0]
+}
+
+async function createUser(email, name, surname, password){
+	await db.query(`insert into users values ("${email}", "${name}", "${surname}", "${password}");`)
+}
+
+async function updateUser(email, password){
+	await db.query(`update users set password="${password}" where email="${email}";`)
+}
+const middleware = {checkUser, getUser, updateUser, createUser}
+
+export default middleware
--- server.js
+++ server.js
@@ -1,97 +1,95 @@
 //Initializations
-const express = require("express")
-const path = require("path")
-const jwt = require("jsonwebtoken")
-const fs = require("fs")
-const randomstring = require("randomstring")
-
-require("dotenv").config()
+import express from "express"
+import jwt from "jsonwebtoken"
+import randomstring from "randomstring"
+import dotenv from "dotenv"
+dotenv.config()
 
 const server = express()
 server.use(express.urlencoded({extended:false}))
 server.use(express.json())
 
-var userList = fs.readFileSync("./user_list.json")
-userList = JSON.parse(userList)
-
 //Functions
 
-const middleware = require("./project/middleware.js")
+import middleware from "./project/middleware.js"
 
 //Server Methods
 server.get("/api/v1/auth", middleware.checkUser, (req, res)=>{
 	if(req.user){
+		console.log("oh wow")
 		return res.status(200).json(req.user.email);
 	}
-	else {return res.status(200);
+	else {
+		console.log("oh wow")
+		return res.status(200).end();
 	}
 })
 
-server.post("/api/v1/auth/sign-in", (req, res)=>{
+server.post("/api/v1/auth/sign-in", async(req, res)=>{
 	const {email, password} = req.body
 	const user = {email, password}
-	const x = userList.find(x => x.email === user.email)
-	if(x){
+	var x = await middleware.getUser(user.email)
+	if(x.length){
+		x=x[0]
 		if(x.password === user.password){
 			const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
 			res.set("content-type", "text/plain")
 			res.status(200).json(accessToken)
 		}
 		else{
-			res.status(403)
+			res.status(403).end()
 		}
 	}
 	else{
-		res.status(403)
+		res.status(403).end()
 	}
 })
 
-server.post("/api/v1/auth/sign-up", (req, res)=>{
-	const {email, password, confirm} = req.body
-	const user = {email, password}
-	const x = userList.find(x => x.email === user.email)
+server.post("/api/v1/auth/sign-up", async(req, res)=>{
+	const {email, password, confirm, name, surname} = req.body
+	var user = {email, password, name, surname}
+	var x = await middleware.getUser(user.email)
 	
-	if(!x){
+	if(!x.length){
 		if(password === confirm){
-			userList.push(user)
-			middleware.updateJSON(userList)
-			res.status(200).json(user)
+			await middleware.createUser(user.email, user.name, user.surname, user.password)
+			user = await middleware.getUser(user.email)
+			res.status(200).json(user[0])
 		}
 		else{
-			res.status(403)
+			res.status(403).end()
 		}
 	}
 	else{
-		res.status(403)
+		res.status(403).end()
 	}
 })
 
-server.post("/api/v1/auth/reset-password", (req, res)=>{
-	console.log(req.body.email)
-	console.log(userList.findIndex(x=>x.email === req.body.email))
-	var index = userList.findIndex(x=>x.email === req.body.email)
-	if(index !== -1){
+server.post("/api/v1/auth/reset-password", async(req, res)=>{
+	const index = await middleware.getUser(req.body.email)
+	if(index.length){
 		const tempPass = randomstring.generate(16)
-		userList[index].password = tempPass
-		middleware.updateJSON(userList)
-		res.status(200).json(userList[index])
+		await middleware.updateUser(req.body.email, tempPass)
+		var user = await middleware.getUser(req.body.email)
+		res.status(200).json(user[0])
 	}
 	else{
-		res.status(403)
+		res.status(403).end()
 	}
 })
 
-server.post("/api/v1/auth/update-password", middleware.checkUser, (req, res)=>{
-	if (req.user & req.body.password === req.body.confirm){
-		userList[userList.findIndex(x=>x.email === req.user.email)].password = req.body.password
-		middleware.updateJSON(userList)
-		res.status(200)
+server.post("/api/v1/auth/update-password", middleware.checkUser, async(req, res)=>{
+	console.log(req.user, "\n", req.body.password, "\n", req.body.confirm)
+	console.log(Boolean(req.user && (req.body.password == req.body.confirm)))
+	if (req.user && req.body.password == req.body.confirm){
+		await middleware.updateUser(req.user.email, req.body.password)
+		res.status(200).end()
 	}
 	else if (!req.user){
-		res.status(403)
+		res.status(403).end()
 	}
 	else{
-		res.status(403)
+		res.status(403).end()
 	}
 })
 
